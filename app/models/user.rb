1ea76bf70bb83fc517bf72e713838fd88b6fd889
Chase Bristow <cbristow26@gmail.com>
2016-10-29 05:00:12 +0000

    Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :grams
+  has_many :comments
 
 end
